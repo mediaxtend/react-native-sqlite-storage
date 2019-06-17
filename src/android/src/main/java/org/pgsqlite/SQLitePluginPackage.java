f7e702557b0c0c107d30fa185b3234b0862f5df2
Nicolas SANDRI (MediaXtend) <contact@mediaxtend.net>
2019-06-17 15:42:52 +0200

    Update package class to new implementation

--- src/android/src/main/java/org/pgsqlite/SQLitePluginPackage.java
+++ src/android/src/main/java/org/pgsqlite/SQLitePluginPackage.java
@@ -5,10 +5,7 @@
  */
 package org.pgsqlite;
 
-import android.app.Activity;
-
 import com.facebook.react.ReactPackage;
-import com.facebook.react.bridge.JavaScriptModule;
 import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
@@ -22,15 +19,9 @@ import java.util.List;
 
 public class SQLitePluginPackage implements ReactPackage {
 
-    /**
-     * @deprecated, use method without activity
-     * activity parameter is ignored
-     */
-    public SQLitePluginPackage(Activity activity){
-        this();
-    }
-
-    public SQLitePluginPackage() {
+    @Override
+    public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
+        return Collections.emptyList();
     }
 
     @Override
@@ -42,15 +33,5 @@ public class SQLitePluginPackage implements ReactPackage {
       modules.add(new SQLitePlugin(reactContext));
 
       return modules;
-    }
-
-    @Override
-    public List<Class<? extends JavaScriptModule>> createJSModules() {
-        return Collections.emptyList();
-    }
-
-    @Override
-    public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
-      return Collections.emptyList();
     }
 }
